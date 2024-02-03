d2874d8ec2a0920eefa807708f152d8c73318a90
Kevin Hogan <kchogan@pacbell.net>
2024-02-03 13:15:06 -0800

    added gets for all 3 routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,8 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+// get all categories
 router.get('/', async (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
       include: [{ model: Product }],
@@ -16,9 +15,8 @@ router.get('/', async (req, res) => {
   }  
 });
 
+// get one category
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
@@ -35,16 +33,19 @@ router.get('/:id', async (req, res) => {
   }  
 });
 
+// create a new category
 router.post('/', async (req, res) => {
-  // create a new category
+
 });
 
+// update a category
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
+
 });
 
+// delete a category by its `id` value
 router.delete('/:id', async (req, res) => {
-  // delete a category by its `id` value
+
 });
 
 module.exports = router;
