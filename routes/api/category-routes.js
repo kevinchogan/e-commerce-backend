93eac2b20f9423286fa16ef74f5e53db6f95dec4
Kevin Hogan <kchogan@pacbell.net>
2024-02-03 13:35:44 -0800

    completed post routes for category and tag

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -35,7 +35,12 @@ router.get('/:id', async (req, res) => {
 
 // create a new category
 router.post('/', async (req, res) => {
-
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // update a category
